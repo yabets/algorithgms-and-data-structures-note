ffdc501b9d4b92eb3b34ca30d4897628cd04d512
Yabi <yabetsbelay@gmail.com>
2026-02-11 04:59:41 +0000

    NumArray

---
+++ prefix_sum/num_array.rs
@@ -0,0 +1,31 @@
+// source https://leetcode.com/problems/range-sum-query-immutable
+struct NumArray {
+    sum: Vec<i32>,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumArray {
+
+    fn new(nums: Vec<i32>) -> Self {
+        let mut sum = vec![0; nums.len()+1];
+        for i in 0..nums.len() {
+            sum[i+1] = sum[i] + nums[i];
+        }
+        // println!("{:?}", sum);
+        NumArray { sum }
+    }
+    
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.sum[right as usize + 1] - self.sum[left as usize]
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * let obj = NumArray::new(nums);
+ * let ret_1: i32 = obj.sum_range(left, right);
+ */
