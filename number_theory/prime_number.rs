e83caa7df32651063f7284819a9030f911cc1210
Yabi <yabetsbelay@gmail.com>
2026-02-21 10:34:01 +0000

    generate prime numbers

--- number_theory/prime_number.rs
+++ number_theory/prime_number.rs
@@ -12,3 +12,20 @@ pub fn is_prime(n: i32) -> bool {
     }
     return true;
 }
+
+pub fn generate_prime() -> Vec<bool> {
+    let mut is_prime: [bool; 128] = [true; 128];
+    // map primes
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for ix in 2..128_i32.isqrt() as usize {
+        if is_prime[ix] {
+                // println!("{ix} is prime");
+            for iy in ((ix * ix)..128).step_by(ix) {
+                    // print!(" {iy} ");
+                is_prime[iy] = false;
+            }
+        }
+            // println!("");
+    }
+}
